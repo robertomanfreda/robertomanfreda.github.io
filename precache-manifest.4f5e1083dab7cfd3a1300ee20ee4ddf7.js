ea61f13a193d5dcc4fb264da41451aa32d35781d
Roberto Manfreda <robertomanfreda1994@gmail.com>
2020-04-20 12:26:10 +0200

    Updates

---
+++ precache-manifest.4f5e1083dab7cfd3a1300ee20ee4ddf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ca3c36261a98374526a92bc38b572d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebe9eb1a4e69e5335287",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5c70b30c7bc681f5e46e",
+    "url": "/static/js/2.69a19bdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.69a19bdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebe9eb1a4e69e5335287",
+    "url": "/static/js/main.522a5cd0.chunk.js"
+  },
+  {
+    "revision": "5baf421140d23857c4f7",
+    "url": "/static/js/runtime-main.4a74c043.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
